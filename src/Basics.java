5f08b8f44846e6b94bdc8693ed7a4d322f4c48a7
Rahul_Chahar <rahul.kumar_cs19@gla.ac.in>
2022-05-01 19:35:03 +0530

    Start

---
+++ src/Basics.java
@@ -0,0 +1,21 @@
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+
+
+        System.out.println(Short.SIZE);
+        System.out.println(Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+
+        System.out.println(Double.SIZE);
+        System.out.println(Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+
+        System.out.println(Float.SIZE);
+        System.out.println(Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+
+
+    }
+}
